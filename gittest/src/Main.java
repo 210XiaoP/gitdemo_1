abd5924828ca1708ee5ee038c876ed8a42b65c5d
panchun <panchunyz@seu.edu.cn>
2025-04-03 17:05:39 +0800

    pc 次修改

--- gittest/src/Main.java
+++ gittest/src/Main.java
@@ -1,9 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("做了一次修改");
-        System.out.println("第二次修改");
-        System.out.println("第三次修改");
-        System.out.println("第lzyyjhyypki修改");
+        System.out.println("main");
+
     }
 }
